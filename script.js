804badd4f5b5f7ff33d2584916fb23ba152a041a
Proutum-Ann <liangproutum@gmail.com>
2025-10-15 10:36:10 -0400

    idk (actually)

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+const schedulePick = document.getElementById.value
 async function loadData() {
 	try {
 		const aSched = await fetch("annieSchedule.json")
 		const data1 = await aSched.json()
 
+        if (schedulePick === "annie")
 		document.getElementById("out").innerHTML = `
-			<h2>${data1.name}</h2>
-			<p>${data1.role}</p>
+			<h2>${data1.className}</h2>
+			<p>${data1.period}</p>
 		`
 	} catch {
 		document.getElementById("out").textContent = "Error loading data."
