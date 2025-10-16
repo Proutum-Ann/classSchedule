c15e34db1c1317fee3c4ba105e75f81d6cb7d048
Proutum-Ann <liangproutum@gmail.com>
2025-10-16 11:01:29 -0400

    new json

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 const schedulePick = document.getElementById.value
 async function loadData() {
 	try {
-		const aSched = await fetch("annieSchedule.json")
-		const data1 = await aSched.json()
-
-        if (schedulePick === "annie")
-		document.getElementById("out").innerHTML = `
-			<h2>${data1.className}</h2>
-			<p>${data1.period}</p>
-		`
+		const ann = fetch("annieSchedule.json")
+		const layl = fetch("laylaSchedule.json")
 	} catch {
-		document.getElementById("out").textContent = "Error loading data."
-		console.error("Async error:", err)
+		
 	}
 }
 
