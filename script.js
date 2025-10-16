e45823ea27e2bbfcb5de976feab4c0c0ad3caaa3
Proutum-Ann <liangproutum@gmail.com>
2025-10-16 11:46:22 -0400

    redoing js

--- script.js
+++ script.js
@@ -1,44 +1,14 @@
-async function loadData() {
-	try {
+let jsonFile = ''
 
-		var schedule = ''
-		const output = document.getElementById('out')
-		output.innerHTML = ''
-		const dropdown = document.getElementById('scheduleList')
+fetch("annieSchedule.json")
+	.then((response) => response.json())
+	.then((data) => {
+		jsonFile = data
 
-		const ann = await fetch("annieSchedule.json")
-		const dataAnn = await ann.json()
-		console.log(dataAnn)
-
-		const layl = fetch("laylaSchedule.json")
-		const dataLayl = await layl.json()
-		console.log(dataLayl)
-
-		dropdown.addEventListener("change", function(event) {
-			const person = event.target.value
-			console.log(person)
-
-			if (person === "annie") {
-				schedule = dataAnn
-			} else if (person === "layla") {
-				schedule = dataLayl
-			}
-
-			schedule.forEach(s => {
-				const cards = document.createElement('div')
-
-				cards.classList = "col-6 col-md-4 col-xl-3 my-2"
-
-				cards.innerHTML = `<p>${s.period}</p>`
-
-				output.appendChild(cards)
-			})
-
+		jsonFile.forEach(classes => {
+			
+			document.getElementById('out').innerHTML = `<p>${classes.period}</p>`
 		})
+	})
 
-	} catch {
-
-	}
-}
-
-loadData()
+/* document.getElementById().insertAjacentHTML("afterend", stuff here) */
