7d96b902150bcb9f16f4d3424e93d9f8352cf800
Proutum-Ann <liangproutum@gmail.com>
2025-10-16 11:28:18 -0400

    im gonna rip my hair out

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-const schedulePick = document.getElementById.value
 async function loadData() {
 	try {
 
 		var schedule = ''
-		const ann = fetch("annieSchedule.json")
+		const output = document.getElementById('out')
+		output.innerHTML = ''
+		const dropdown = document.getElementById('scheduleList')
+
+		const ann = await fetch("annieSchedule.json")
+		const dataAnn = await ann.json()
+		console.log(dataAnn)
+
 		const layl = fetch("laylaSchedule.json")
+		const dataLayl = await layl.json()
+		console.log(dataLayl)
 
-		if (schedulePick === "annie") {
-			schedule = ann
-		} else if (schedulePick === "layla") {
-			schedule = layl
-		}
+		dropdown.addEventListener("change", function(event) {
+			const person = event.target.value
+			console.log(person)
 
-		schedule.forEach(s =>
-			h
-		)
+			if (person === "annie") {
+				schedule = dataAnn
+			} else if (person === "layla") {
+				schedule = dataLayl
+			}
+
+			schedule.forEach(s => {
+				const cards = document.createElement('div')
+
+				cards.classList = "col-6 col-md-4 col-xl-3 my-2"
+
+				cards.innerHTML = `<p>${s.period}</p>`
+
+				output.appendChild(cards)
+			})
+
+		})
 
 	} catch {
 
