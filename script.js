b59581526946f623848c63b8d2df0b2dd2bfc288
Proutum-Ann <liangproutum@gmail.com>
2025-10-16 11:07:22 -0400

    edited the select a bit

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const schedulePick = document.getElementById.value
 async function loadData() {
 	try {
+
+		var schedule = ''
 		const ann = fetch("annieSchedule.json")
 		const layl = fetch("laylaSchedule.json")
+
+		if (schedulePick === "annie") {
+			schedule = ann
+		} else if (schedulePick === "layla") {
+			schedule = layl
+		}
+
+		schedule.forEach(s =>
+			h
+		)
+
 	} catch {
-		
+
 	}
 }
 
