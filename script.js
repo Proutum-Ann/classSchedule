df472d074992337d17a2c5040b90623f8b9e1a44
Proutum-Ann <liangproutum@gmail.com>
2025-10-20 09:45:19 -0400

    added text colors for card headers

--- script.js
+++ script.js
@@ -18,26 +18,35 @@ async function loadSchedule(fileName) {
         data.forEach(classes => {
 
 			let headerBG = ''
+			let headerText = ''
 			let cardBG = ''
 
 			//Change colors based off of subject
 
 			if (classes.subjectArea === "Math") {
-				headerBG = '#264eff'
+				headerBG = '#5356ff'
+				headerText = '#ddecff'
 			} else if (classes.subjectArea === "English") {
 				headerBG = '#2dff1a'
+				headerText = '#047223'
 			} else if (classes.subjectArea === "Science") {
 				headerBG = '#3fffff'
+				headerText = '#0c7785'
 			} else if (classes.subjectArea === "Technology") {
 				headerBG = '#a9a2ff'
+				headerText = '#141eac'
 			} else if (classes.subjectArea === "Physical Education" || classes.subjectArea === "Health") {
 				headerBG = '#81ff76'
+				headerText = '#168036'
 			} else if (classes.subjectArea === "Visual Arts" || classes.subjectArea === "Performing Arts") {
 				headerBG = '#ecff40'
+				headerText = '#ad4e00'
 			} else if (classes.subjectArea === "Social Studies") {
 				headerBG = '#91ca85'
+				headerText = '#4b5c24'
 			} else {
 				headerBG = '#8d8d8d'
+				headerText = '#fff'
 			}
 
 			//change color based off of hallway
@@ -59,7 +68,7 @@ async function loadSchedule(fileName) {
             const classInfo = `
 				<div class="col-12 col-md-4 my-2">
 					<div class="card border-10 px-2 py-1 h-100" style="background: ${cardBG}">
-						<h5 class="card-header border-0" style="background-color: ${headerBG}; border-radius: 10px">${classes.className}</h5>
+						<h5 class="card-header border-0" style="background-color: ${headerBG}; color: ${headerText}; border-radius: 10px">${classes.className}</h5>
 						<p class="card-text p-2">
 							<b>Period ${classes.period}</b><br>
 							<b>Teacher:</b> ${classes.teacher.join(', ')}<br>
@@ -70,7 +79,6 @@ async function loadSchedule(fileName) {
 				</div>
             `;
             output.insertAdjacentHTML("beforeend", classInfo);
-
         });
     } catch (error) {
         console.error("Error loading schedule:", error);
@@ -85,3 +93,10 @@ document.getElementById('studentList').addEventListener('change', (event) => {
     const selected = event.target.value;
     loadSchedule(selected);
 });
+
+//keydown stuff
+addEventListener("keydown", (event) => {
+	h
+})
+
+onkeydown = (event) => { }
